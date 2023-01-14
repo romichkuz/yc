40b05bf0eda392773bae1ab7d9bf30adc6c83aa5
Roman Kuzmin <romichkuz@mail.ru>
2023-01-14 17:53:12 +0500

    almost ready

--- back/app.py
+++ back/app.py
@@ -74,5 +74,5 @@ def handle_notes_queries():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get('APP_PORT', 5000))
+    port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
--- back/update_version.sh
+++ back/update_version.sh
@@ -4,9 +4,26 @@ export $(cat version.txt | xargs)
 NEXT_VERSION=$(echo $APP_VERSION | awk -F. -v OFS=. '{$NF += 1 ; print}')
 echo "APP_VERSION=${NEXT_VERSION}" > version.txt
 
-IMAGE_NAME=${APP_REPOSITORY}:$NEXT_VERSION
+APP_REPOSITORY=$(echo ${APP_REPOSITORY})
+IMAGE_NAME=$(echo "${APP_REPOSITORY}:${NEXT_VERSION}")
 
+echo $IMAGE_NAME
 docker build -t $IMAGE_NAME . ;
 
-# docker push
-# yls deploy 
+docker push $IMAGE_NAME;
+
+CONTAINER_ID=$(echo ${CONTAINER_ID})
+YDB_ENDPOINT=$(echo ${YDB_ENDPOINT})
+YDB_DATABASE=$(echo ${YDB_DATABASE})
+SA_ID=$(echo ${SA_ID})
+
+
+yc sls container revisions deploy \
+    --container-id ${CONTAINER_ID} \
+    --memory 512M \
+    --cores 1 \
+    --execution-timeout 5s \
+    --concurrency 8 \
+    --image "$IMAGE_NAME" \
+    --environment  YDB_ENDPOINT=${YDB_ENDPOINT},YDB_DATABASE=${YDB_DATABASE} \
+    --service-account-id ${SA_ID};
